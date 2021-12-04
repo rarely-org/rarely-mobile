7324fd4064eb43ce7cb790effe0972c111d941fe
Steffen Hiller <6301+steffen@users.noreply.github.com>
2021-12-04 10:50:55 +0000

    Add "Discover" and "Search" view & bottom tab navigation

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Discover = () => (
+  <View><Text>Discover</Text></View>
+)
+
+const Search = () => (
+  <View><Text>Search</Text></View>
+)
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Discover" component={Discover} />
+    <Tab.Screen name="Search" component={Search} />
+  </Tab.Navigator>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
   );
 }
 

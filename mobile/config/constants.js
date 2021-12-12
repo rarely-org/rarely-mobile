ebccc5fdf3570b592f9eedf31c127ad1eed921be
Steffen Hiller <6301+steffen@users.noreply.github.com>
2021-12-12 17:01:01 +0000

    Add loading list of posts from Supabase

---
+++ mobile/config/constants.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = process.env.SUPABASE_URL || ''
+export const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || ''
--- mobile/screens/DiscoverScreen.js
+++ mobile/screens/DiscoverScreen.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 import Card from "../components/Card";
 import colors from "../config/colors";
 import Screen from "../components/Screen";
+import { supabase } from '../config/supabase';
 
-const posts = [
-  {
-    id: 1,
-    text: "Mio from SORG",
-  },
-  {
-    id: 2,
-    text: "F5 Corpus VS from Permobil",
-  },
-];
+// URL polyfill. Required for Supabase queries to work in React Native.
+import 'react-native-url-polyfill/auto'
 
 function DiscoverScreen({ navigation }) {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  const fetchPosts = async () => {
+    const { data: posts, error } = await supabase
+      .from('posts')
+      .select('*')
+      .order('id', { ascending: false })
+
+    if (error) console.log('error', error)
+    else setPosts(posts)
+  }
+
   return (
     <Screen style={styles.screen}>
       <FlatList

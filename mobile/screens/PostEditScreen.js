6bf320b938ff69c04c2da7d020dfb0bf4e1537c9
Steffen Hiller <6301+steffen@users.noreply.github.com>
2021-12-20 20:41:40 +0100

    Add create post screen and save in supabase (#21)
    * First version of create post screen
    
    * Create post in supabase

---
+++ mobile/screens/PostEditScreen.js
@@ -0,0 +1,69 @@
+import React from "react"
+import { StyleSheet, TextInput } from "react-native"
+import { Formik, useFormikContext } from "formik";
+import { supabase } from '../config/supabase';
+
+import Screen from "../components/Screen"
+import Button from "../components/Button"
+
+const createPost = async (text) => {
+  text = text.trim()
+
+  if (text.length) {
+    const { data: post, error } = await supabase
+      .from('posts')
+      .insert({ text })
+      .single()
+
+    if (error) console.log(error.message)
+    else {}
+  }
+}
+
+const TextField = () => {
+  const { handleChange } = useFormikContext();
+
+  return (
+    <TextInput
+      maxLength={1000}
+      multiline
+      numberOfLines={3}
+      placeholder="Add a text"
+      onChangeText={handleChange("text")}
+    />
+  )
+}
+
+const SubmitButton = () => {
+  const { handleSubmit } = useFormikContext();
+
+  return (
+    <Button title="Post" onPress={handleSubmit} />
+  )
+}
+
+function PostEditScreen() {
+  return (
+    <Screen style={styles.container}>
+      <Formik
+        initialValues={{
+          text: "",
+        }}
+        // onSubmit={(values) => console.log(values)}
+        onSubmit={({text}) => createPost(text)}
+      >
+        <>
+          <TextField />
+          <SubmitButton />
+        </>
+      </Formik>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
+export default PostEditScreen;

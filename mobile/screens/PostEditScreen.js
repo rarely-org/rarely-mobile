7f038a116915cf64038a507d11832d74014c8def
Steffen Hiller <6301+steffen@users.noreply.github.com>
2021-12-20 17:10:09 +0000

    Create post in supabase

--- mobile/screens/PostEditScreen.js
+++ mobile/screens/PostEditScreen.js
@@ -1,9 +1,38 @@
 import React from "react"
 import { StyleSheet, TextInput } from "react-native"
 import { Formik, useFormikContext } from "formik";
+import { supabase } from '../config/supabase';
 
 import Screen from "../components/Screen"
 import Button from "../components/Button"
+
+const createPost = async (text) => {
+  text = text.trim()
+
+  if (text.length) {
+    const { data: post, error } = await supabase
+      .from('posts')
+      .insert({ text })
+      .single()
+
+    if (error) console.log(error.message)
+    else {}
+  }
+}
+
+const TextField = () => {
+  const { handleChange } = useFormikContext();
+
+  return (
+    <TextInput
+      maxLength={1000}
+      multiline
+      numberOfLines={3}
+      placeholder="Add a text"
+      onChangeText={handleChange("text")}
+    />
+  )
+}
 
 const SubmitButton = () => {
   const { handleSubmit } = useFormikContext();
@@ -20,16 +49,11 @@ function PostEditScreen() {
         initialValues={{
           text: "",
         }}
-        onSubmit={(values) => console.log(values)}
+        // onSubmit={(values) => console.log(values)}
+        onSubmit={({text}) => createPost(text)}
       >
         <>
-          <TextInput
-            maxLength={1000}
-            multiline
-            name="text"
-            numberOfLines={3}
-            placeholder="Add a text"
-          />
+          <TextField />
           <SubmitButton />
         </>
       </Formik>

88dd772e3ccf411abacff6577728d503cf4d4e76
Steffen Hiller <6301+steffen@users.noreply.github.com>
2021-12-21 20:46:24 +0000

    Styled and reorganized tabs

--- mobile/App.js
+++ mobile/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 import Screen from "./components/Screen";
 import DiscoverScreen from "./screens/DiscoverScreen";
 import PostEditScreen from "./screens/PostEditScreen";
@@ -11,12 +13,36 @@ const Search = () => (
   <Screen><Text>Search</Text></Screen>
 )
 
+const MyPosts = () => (
+  <Screen><Text>My posts</Text></Screen>
+)
+
+const Profile = () => (
+  <Screen><Text>Profile</Text></Screen>
+)
+
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Discover" component={DiscoverScreen} />
-    <Tab.Screen name="Add" component={PostEditScreen} />
-    <Tab.Screen name="Search" component={Search} />
+  <Tab.Navigator tabBarOptions={{
+    showLabel: false,
+    activeTintColor: "#2196f3",
+    inactiveTintColor: "#333333"
+  }}>
+    <Tab.Screen name="Discover" component={DiscoverScreen} options={{
+      tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="star" color={color} size={size} />)
+    }}/>
+    <Tab.Screen name="Search" component={Search} options={{
+      tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="magnify" color={color} size={size} />)
+    }}/>
+    <Tab.Screen name="Add" component={PostEditScreen} options={{
+      tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="plus-circle" color={color} size={size} />)
+    }}/>
+    <Tab.Screen name="MyPosts" component={MyPosts} options={{
+      tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="format-list-bulleted-square" color={color} size={size} />)
+    }}/>
+    <Tab.Screen name="Profile" component={Profile} options={{
+      tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="account" color={color} size={size} />)
+    }}/>
   </Tab.Navigator>
 )
 
